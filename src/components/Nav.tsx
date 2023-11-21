3aba4d5cf7c962a05dd5e2639049a57f5930f648
VICTOR-CHUKWU <okenwa1993@gmail.com>
2023-11-21 11:38:20 +0100

    add skeleton

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,13 @@
-import React, { memo, useCallback } from 'react'
+'use client'
+import React, { memo, useCallback, useState } from 'react'
 import { navMenuItems } from '@/data'
 
 const _Nav = () => {
+    const [isMenu, setIsMenu] = useState(false)
     const renderNav = useCallback(
         ({ url, title }: { url: string, title: string }) => {
             return (
-                <span className=' nav-menu-item text-lg uppercase cursor-pointer'>{title}</span>
+                <span onClick={() => setIsMenu(!isMenu)} className=' nav-menu-item text-base uppercase cursor-pointer'>{title}</span>
             )
         },
         [],
@@ -15,7 +17,7 @@ const _Nav = () => {
         <div className=' bg-transparent py-8 fixed w-full left-0 top-0'>
             <nav className=' container flex items-center justify-between Nav'>
                 <p className=' text-red-900 font-bold text-xl'>Logo</p>
-                <div className=' flex items-center gap-6'>
+                <div className=' flex items-center gap-10'>
                     {navMenuItems.map(renderNav)}
                 </div>
             </nav>
---
+++ src/components/shared/navSubMenu.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+const _navSubMenu = () => {
+    return (
+        <div>navSubMenu</div>
+    )
+}
+
+export const navSubMenu = memo(_navSubMenu)
